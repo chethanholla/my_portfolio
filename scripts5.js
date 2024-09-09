51adfa30f148989155020f276b655e440299c52a
Chethan Holla BV <chethanholla1@gmail.com>
2024-09-09 10:57:13 +0530

    update changes to final product

--- scripts5.js
+++ scripts5.js
@@ -273,7 +273,6 @@ document.addEventListener('DOMContentLoaded', () => {
         currentPage = 1;
         filteredProjects = [];
         document.getElementById('category-heading').textContent = 'Projects';
-        loadCertificates();
     }
 });
 //--------------------------------------------------------------------------------------------
